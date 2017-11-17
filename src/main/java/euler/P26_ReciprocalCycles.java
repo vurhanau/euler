3f6e981ecc9a38874b6ae715348d13e8e8cd37a6
Alexander Vurhanau <alexander.vurhanau@siliconmint.com>
2017-11-17 15:38:35 +0300

    p26

--- src/main/java/euler/P26_ReciprocalCycles.java
+++ src/main/java/euler/P26_ReciprocalCycles.java
@@ -8,59 +8,38 @@ public class P26_ReciprocalCycles {
     public static void main(String[] args) {
         int n = 1000;
         int max = Integer.MIN_VALUE;
+        int nmax = -1;
         for (int i = 1; i < n; i++) {
             int cycle = cycle(i);
             if (cycle > max) {
-                System.out.println(i + ": " + cycle);
                 max = cycle;
+                nmax = i;
             }
         }
-        System.out.println(max);
+        System.out.println(nmax);
     }
 
     private static int cycle(int d) {
         int mod = 1;
         List<Integer> digits = new ArrayList<>();
         List<Integer> cycles = new ArrayList<>();
-        int[] cycle = {-1, -1};
-        int[] prevCycle = {-1, -1};
-        while (mod != 0 && cycles.size() < 10) {
+        while (mod != 0 && cycles.size() < 10000) {
             mod *= 10;
-            while (mod < d) {
-                digits.add(0);
-                mod *= 10;
-            }
             digits.add(mod / d);
             mod %= d;
 
-            int[] c = cycle0(cycle[1] + 1, digits);
-            if (c[1] > 0) {
-                prevCycle = cycle;
-                cycle = c;
-                if (prevCycle[0] > 0 && isConcat(prevCycle, cycle, digits)) {
-                    return prevCycle[1];
-                }
+            int cycle = cycle0(digits);
+            if (cycle > 0) {
+                cycles.add(cycle);
             }
         }
 
-        return 0;
+        return cycles.isEmpty() ? 0 : cycles.stream().mapToInt(i -> i).max().getAsInt();
     }
 
-    private static boolean isConcat(int[] cycle0, int[] cycle1, List<Integer> digits) {
-        for (int i = 0; i < cycle0[1]; i++) {
-            int a = digits.get(cycle1[0] - i);
-            int b = digits.get(cycle0[0] - i);
-            if (a != b) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static int[] cycle0(int minimalLength, List<Integer> digits) {
+    private static int cycle0(List<Integer> digits) {
         int size = digits.size();
-        for (int cycleLength = minimalLength; cycleLength <= size / 2; cycleLength++) {
+        for (int cycleLength = 0; cycleLength <= size / 2; cycleLength++) {
             boolean found = false;
             for (int j = 0; j < cycleLength; j++) {
                 int a = digits.get(size - j - 1);
@@ -72,10 +51,10 @@ public class P26_ReciprocalCycles {
             }
 
             if (found) {
-                return new int[]{digits.size() - 1, cycleLength};
+                return cycleLength;
             }
 
         }
-        return new int[]{-1, 0};
+        return 0;
     }
 }
